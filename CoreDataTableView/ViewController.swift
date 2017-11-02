63be3846e3658e2fe35b1de0c83d0996def55747
NAVRUZ <akhmedovnavruzbek@gmail.com>
2017-11-02 13:33:37 +0500

    Initial Commit

---
+++ CoreDataTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataTableView
+//
+//  Created by NAVRUZ on 11/2/17.
+//  Copyright © 2017 NAVRUZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
